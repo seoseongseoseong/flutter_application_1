3c53cdb2e5836246951ad8728d0db96a59925a87
seoseongseoseong <cube1080@snu.ac.kr>
2024-08-02 18:18:03 +0900

    update

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -8,7 +8,7 @@ const amplifyconfig = '''{
         "Version": "0.1.0",
         "identityPoolId": "YOUR_IDENTITY_POOL_ID",
         "region": "YOUR_REGION",
-        "userPoolId": "YOUR_USER_POOL_ID",
+        "userPoolId": "ap-northeast-2_3yFAGywA2",
         "userPoolWebClientId": "YOUR_USER_POOL_WEB_CLIENT_ID",
         "oauth": {}
       }
